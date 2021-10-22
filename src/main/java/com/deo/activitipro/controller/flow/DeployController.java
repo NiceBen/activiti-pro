edc40330dc6f8c3ef19e4ab99f5f40970763132a
OfBen <jpzhou2017@126.com>
2021-10-22 17:50:47 +0800

    add deploy process files

--- src/main/java/com/deo/activitipro/controller/flow/DeployController.java
+++ src/main/java/com/deo/activitipro/controller/flow/DeployController.java
@@ -1,16 +1,66 @@
 package com.deo.activitipro.controller.flow;
 
+import com.deo.activitipro.utils.RestMessage;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.DeploymentBuilder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * TODO
+ * 部署、删除流程
  *
  * @author SL Zhou
  * @date 2021-10-22
- * @since TODO
+ * @since 1.0.0
  */
 @RestController
 @Api(tags = "部署、删除流程")
 public class DeployController {
+
+    private final RepositoryService repositoryService;
+
+    public DeployController(RepositoryService repositoryService) {
+        this.repositoryService = repositoryService;
+    }
+
+    @PostMapping(path = "/deploy")
+    @ApiOperation(value = "根据bpmnName部署流程", notes = "根据bpmnName部署流程")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "bpmnName", value = "设计的流程图名称", dataType = "String", paramType = "query", example = "myProcess")
+    })
+    public RestMessage deploy(@RequestParam("bpmnName") String bpmnName) {
+        RestMessage restMessage = new RestMessage();
+        // 创建一个部署对象
+        DeploymentBuilder deploymentBuilder = repositoryService.createDeployment().name("请假流程");
+        Deployment deployment = null;
+        try {
+            deploymentBuilder
+                    .addClasspathResource("processes/" + bpmnName + ".bpmn20.xml")
+                    .addClasspathResource("processes/" + bpmnName + ".png")
+                    .deploy();
+        } catch (Exception e) {
+            restMessage = RestMessage.fail("部署失败", e.getMessage());
+            e.printStackTrace();
+        }
+        if (deployment != null) {
+            Map<String, String> result = new HashMap<>(2);
+            result.put("deployId", deployment.getId());
+            result.put("deployName", deployment.getName());
+            restMessage = RestMessage.success("部署成功", result);
+
+        }
+        return restMessage;
+    }
+
+
+
 }
