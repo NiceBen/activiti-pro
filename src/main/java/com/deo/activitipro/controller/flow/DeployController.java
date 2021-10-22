c46fa84443844e6928d34ecd7ab013c022b05d2e
OfBen <jpzhou2017@126.com>
2021-10-22 18:20:27 +0800

    add deploy and delete process definition

--- src/main/java/com/deo/activitipro/controller/flow/DeployController.java
+++ src/main/java/com/deo/activitipro/controller/flow/DeployController.java
@@ -12,8 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.zip.ZipInputStream;
 
 /**
  * 部署、删除流程
@@ -61,6 +63,56 @@ public class DeployController {
         return restMessage;
     }
 
+    @PostMapping(path = "/deployZIP")
+    @ApiOperation(value = "根据ZIP压缩包部署流程", notes = "根据ZIP压缩包部署流程")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "zipName", value = "设计的流程图和图片的压缩包名称", dataType = "String", paramType = "query", example = "myProcess")
+    })
+    public RestMessage deployZIP(@RequestParam("zipName") String zipName) {
+        RestMessage restMessage = new RestMessage();
+        Deployment deployment = null;
+        try (InputStream in = this.getClass().getClassLoader().getResourceAsStream("processes/leaveProcess.zip");
+             ZipInputStream zipInputStream = new ZipInputStream(in)) {
 
+            repositoryService.createDeployment()
+                    .name("请假流程2")
+                    // 指定zip格式的文件完成部署
+                    .addZipInputStream(zipInputStream)
+                    .deploy();
+        } catch (Exception e) {
+            restMessage = RestMessage.fail("部署失败", e.getMessage());
+            // TODO -- 上线时移除
+            e.printStackTrace();
+        }
+        if (deployment != null) {
+            Map<String, String> result = new HashMap<>();
+            result.put("deployId", deployment.getId());
+            result.put("deployName", deployment.getName());
+            restMessage = RestMessage.success("部署成功", result);
+        }
+        return restMessage;
+    }
 
+    @PostMapping(path = "/deleteProcess")
+    @ApiOperation(value = "根据部署ID删除流程", notes = "根据部署ID删除流程")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "deploymentId", value = "部署ID", dataType = "String", paramType = "query", example = "myProcess")
+    })
+    public RestMessage deleteProcess(@RequestParam("deploymentId") String deploymentId) {
+        RestMessage restMessage = new RestMessage();
+        /** 不带级联的删除：只能删除没有启动的流程，如果流程启动，就会抛出异常 */
+        try {
+            repositoryService.deleteDeployment(deploymentId);
+        } catch (Exception e) {
+            restMessage = RestMessage.fail("删除失败", e.getMessage());
+            // TODO -- 上线时移除
+            e.printStackTrace();
+        }
+
+        /** 级联删除：不管流程是否启动，都能可以删除 */
+//        repositoryService.deleteDeployment(deploymentId, true);
+
+        restMessage = RestMessage.success("删除成功", null);
+        return restMessage;
+    }
 }
